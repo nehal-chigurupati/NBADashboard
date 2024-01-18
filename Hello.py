e2f6a7178b6f696d3157947bbef50009c1ea1555
Nehal Chigurupati <nehalchigurupati@Nehals-MacBook-Air.local>
2024-01-18 13:53:25 -0500

    Update main page

--- Hello.py
+++ Hello.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.header("Welcome to my dashboard for basketball-related data.")
 
 msg = """
-There are a few endpoints here. Player basic stats gives seasonal data like ppg and apg, 
-         while estimated metrics gives offensive/defensive/net ratings and ranks. Player and team info give the NBA's IDs for each entity.
+This is a work in progress. For player stats, see the player dashboard page. 
 """
 
 st.write(msg)
